acc5864263e40da44a4386251eb5e5260563e47a
crimsonwoods <crimsonwoods@gmail.com>
2012-06-25 00:34:19 +0900

    add utility functions.

---
+++ jni/jamruby_mruby_utils.cpp
@@ -0,0 +1,32 @@
+#include "jamruby_mruby_utils.h"
+extern "C" {
+#include "mruby.h"
+#include "mruby/proc.h"
+#include "mruby/class.h"
+}
+
+char const *get_called_mrb_func_name(mrb_state *mrb)
+{
+	return mrb_sym2name(mrb, mrb->ci->mid);
+}
+
+RProc *replace_mrb_func(mrb_state *mrb, RClass *klass, char const * const name, mrb_func_t func)
+{
+	mrb_sym const mid = mrb_intern(mrb, name);
+	khash_t(mt) * const h = klass->mt;
+	khiter_t k = kh_get(mt, h, mid);
+	if ((k != kh_end(h)) && kh_exist(h, k)) {
+		RProc * const proc = kh_value(h, k);
+		if (NULL != proc) {
+			RProc *newproc = mrb_proc_new_cfunc(mrb, func);
+			if (NULL == newproc) {
+				return NULL;
+			}
+			newproc->target_class = klass;
+			kh_value(h, k) = newproc;
+			return proc;
+		}
+	}
+	return NULL;
+}
+
---
+++ jni/jamruby_mruby_utils.h
@@ -0,0 +1,21 @@
+#ifndef JAMRUBY_MRUBY_UTILS_H
+#define JAMRUBY_MRUBY_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mruby.h"
+
+struct RProc;
+struct RClass;
+
+extern char const   *get_called_mrb_func_name(mrb_state *mrb);
+extern struct RProc *replace_mrb_func(mrb_state *mrb, struct RClass *klass, char const * const name, mrb_func_t func);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // end of JAMRUBY_MRUBY_UTILS_H
+
